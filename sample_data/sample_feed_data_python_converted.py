bc4b65e03075829c417e8ccd6e5eeb60caa17678
Anthony N <neoterix@users.noreply.github.com>
2017-09-01 10:32:31 -0400

    initial commit

---
+++ sample_data/sample_feed_data_python_converted.py
@@ -0,0 +1,446 @@
+{
+    'header':
+    {
+        'gtfs_realtime_version': '1.0',
+        'incrementality': 0,
+        'timestamp': 1503156587
+    },
+    'entity': [
+    {
+        'id': '22000001',
+        'trip_update':
+        {
+            'trip':
+            {
+                'trip_id': '067250_D..S',
+                'start_date': '20170819',
+                'route_id': 'D'
+            },
+            'stop_time_update': [
+            {
+                'arrival':
+                {
+                    'time': 1503155670
+                },
+                'departure':
+                {
+                    'time': 1503155670
+                },
+                'stop_id': 'D03S',
+                'schedule_relationship': 0
+            }]
+        }
+    },
+    {
+        'id': '22000002',
+        'vehicle':
+        {
+            'trip':
+            {
+                'trip_id': '067250_D..S',
+                'start_date': '20170819',
+                'route_id': 'D'
+            },
+            'current_stop_sequence': 1,
+            'current_status': 1,
+            'timestamp': 1503155550
+        }
+    },
+    {
+        'id': '22000003',
+        'trip_update':
+        {
+            'trip':
+            {
+                'trip_id': '067500_D..N',
+                'start_date': '20170819',
+                'route_id': 'D'
+            },
+            'stop_time_update': [
+            {
+                'arrival':
+                {
+                    'time': 1503156510
+                },
+                'departure':
+                {
+                    'time': 1503156510
+                },
+                'stop_id': 'B17N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503156630
+                },
+                'departure':
+                {
+                    'time': 1503156630
+                },
+                'stop_id': 'B16N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503156750
+                },
+                'departure':
+                {
+                    'time': 1503156750
+                },
+                'stop_id': 'B15N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503156840
+                },
+                'departure':
+                {
+                    'time': 1503156840
+                },
+                'stop_id': 'B14N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503156930
+                },
+                'departure':
+                {
+                    'time': 1503156930
+                },
+                'stop_id': 'B13N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503157080
+                },
+                'departure':
+                {
+                    'time': 1503157080
+                },
+                'stop_id': 'B12N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503157320
+                },
+                'departure':
+                {
+                    'time': 1503157320
+                },
+                'stop_id': 'R36N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503157620
+                },
+                'departure':
+                {
+                    'time': 1503157620
+                },
+                'stop_id': 'R31N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503157920
+                },
+                'departure':
+                {
+                    'time': 1503157920
+                },
+                'stop_id': 'Q02N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158340
+                },
+                'departure':
+                {
+                    'time': 1503158340
+                },
+                'stop_id': 'D22N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158460
+                },
+                'departure':
+                {
+                    'time': 1503158460
+                },
+                'stop_id': 'D21N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158580
+                },
+                'departure':
+                {
+                    'time': 1503158580
+                },
+                'stop_id': 'D20N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158670
+                },
+                'departure':
+                {
+                    'time': 1503158670
+                },
+                'stop_id': 'D19N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158760
+                },
+                'departure':
+                {
+                    'time': 1503158760
+                },
+                'stop_id': 'D18N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158850
+                },
+                'departure':
+                {
+                    'time': 1503158850
+                },
+                'stop_id': 'D17N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503158940
+                },
+                'departure':
+                {
+                    'time': 1503158940
+                },
+                'stop_id': 'D16N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503159030
+                },
+                'departure':
+                {
+                    'time': 1503159030
+                },
+                'stop_id': 'D15N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503159120
+                },
+                'departure':
+                {
+                    'time': 1503159120
+                },
+                'stop_id': 'D14N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503159240
+                },
+                'departure':
+                {
+                    'time': 1503159240
+                },
+                'stop_id': 'A24N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503159720
+                },
+                'departure':
+                {
+                    'time': 1503159720
+                },
+                'stop_id': 'A15N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503159975
+                },
+                'departure':
+                {
+                    'time': 1503159975
+                },
+                'stop_id': 'D13N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160140
+                },
+                'departure':
+                {
+                    'time': 1503160140
+                },
+                'stop_id': 'D60N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160200
+                },
+                'departure':
+                {
+                    'time': 1503160200
+                },
+                'stop_id': 'D11N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160350
+                },
+                'departure':
+                {
+                    'time': 1503160350
+                },
+                'stop_id': 'D10N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160470
+                },
+                'departure':
+                {
+                    'time': 1503160470
+                },
+                'stop_id': 'D09N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160590
+                },
+                'departure':
+                {
+                    'time': 1503160590
+                },
+                'stop_id': 'D08N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160725
+                },
+                'departure':
+                {
+                    'time': 1503160725
+                },
+                'stop_id': 'D07N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160860
+                },
+                'departure':
+                {
+                    'time': 1503160860
+                },
+                'stop_id': 'D06N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503160950
+                },
+                'departure':
+                {
+                    'time': 1503160950
+                },
+                'stop_id': 'D05N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503161040
+                },
+                'departure':
+                {
+                    'time': 1503161040
+                },
+                'stop_id': 'D04N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503161160
+                },
+                'departure':
+                {
+                    'time': 1503161160
+                },
+                'stop_id': 'D03N',
+                'schedule_relationship': 0
+            },
+            {
+                'arrival':
+                {
+                    'time': 1503161310
+                },
+                'departure':
+                {
+                    'time': 1503161310
+                },
+                'stop_id': 'D01N',
+                'schedule_relationship': 0
+            }]
+        }
+    }]
+}
